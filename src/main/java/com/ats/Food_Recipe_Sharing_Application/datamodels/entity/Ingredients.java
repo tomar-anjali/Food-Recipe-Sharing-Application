429510c2df12cc724f586cdeb9e558d571a72b43
Amatul <amatulwadood2003@gmail.com>
2024-11-08 17:27:40 +0530

    Ingredients entity and Dto is added

--- src/main/java/com/ats/Food_Recipe_Sharing_Application/datamodels/entity/Ingredients.java
+++ src/main/java/com/ats/Food_Recipe_Sharing_Application/datamodels/entity/Ingredients.java
@@ -1,4 +1,19 @@
 package com.ats.Food_Recipe_Sharing_Application.datamodels.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class Ingredients {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY ,generator = "ingredients_id")
+    @SequenceGenerator(initialValue = 501,name = "ingredients_id",allocationSize = 1)
+    private int ingredientsId;
+    private String ingredientsName;
+    private int quantity;
+    private int unit;
+    private double price;
+
 }
