db2af54ee99cfcce4ae6df5944ba8020c3901b2c
Amatul <amatulwadood2003@gmail.com>
2024-11-09 10:59:54 +0530

    create ingredient API is added

--- src/main/java/com/ats/Food_Recipe_Sharing_Application/dao/IngredientDaoImpl.java
+++ src/main/java/com/ats/Food_Recipe_Sharing_Application/dao/IngredientDaoImpl.java
@@ -1,4 +1,17 @@
 package com.ats.Food_Recipe_Sharing_Application.dao;
 
-public class IngredientDaoImpl {
+import com.ats.Food_Recipe_Sharing_Application.datamodels.entity.Ingredients;
+import com.ats.Food_Recipe_Sharing_Application.repository.IngredientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class IngredientDaoImpl implements IngredientDao{
+    @Autowired
+    private IngredientRepository ingredientRepository;
+
+    @Override
+    public Ingredients createIngredient(Ingredients ingredients) {
+       return ingredientRepository.save(ingredients);
+    }
 }
