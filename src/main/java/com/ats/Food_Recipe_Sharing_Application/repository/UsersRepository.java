afd11d219d19c6c9225ba91e5ada33aaff1f314f
Lakshmi <lakshmibhargavi2002@gmail.com>
2024-11-09 10:46:17 +0530

    createusers API is added

--- src/main/java/com/ats/Food_Recipe_Sharing_Application/repository/UsersRepository.java
+++ src/main/java/com/ats/Food_Recipe_Sharing_Application/repository/UsersRepository.java
@@ -3,7 +3,7 @@ package com.ats.Food_Recipe_Sharing_Application.repository;
 import com.ats.Food_Recipe_Sharing_Application.datamodels.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsersRepository extends JpaRepository<Users, String>
+public interface UsersRepository extends JpaRepository<Users, Integer>
 {
 
 }
